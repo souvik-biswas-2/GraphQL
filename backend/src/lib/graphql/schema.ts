8770df029303fa81ec2c8c7548b1a8d8d791c944
Souvik Biswas <souvik-biswas-2>
2025-12-16 15:30:57 +0530

    Book Created for mutation check 'localhost:port/graphql

--- backend/src/lib/graphql/schema.ts
+++ backend/src/lib/graphql/schema.ts
@@ -3,9 +3,23 @@ import merge from 'lodash.merge' // For merging schemas
 // use .js extension in imports to enable ES module support 
 // otherwise code will not be compliled in production
 import { GlobalTypeDefs } from './global.schema.js'; 
+import { BookResolver, BookTypeDef } from '../../model/book.model.js';
 
-export const typeDefs = [GlobalTypeDefs]; // add other typeDefs in this array as we create more schemas
-export const resolvers = merge({ // merging multiple resolvers
-    Query: {}, // should be same as globaltypeDefs (Query in GlobalTypeDefs and query here ❌)
-    Mutation: {},
-});
+export const typeDefs = [GlobalTypeDefs, BookTypeDef]; // add other typeDefs in this array as we create more schemas
+// export const resolvers = merge({ // merging multiple resolvers
+//     Query: {}, // should be same as globaltypeDefs (Query in GlobalTypeDefs and query here ❌)
+//     Mutation: {},
+// });
+
+
+
+// it is called course splitting  // that why we are using specific version (lodash create conflicts)
+// Course splitting in GraphQL refers to splitting a 
+// large schema into multiple smaller, modular files for 
+// better organization and maintainability.
+// Large schemas become hard to manage. 
+// Splitting separates concerns like 
+// users, products, orders into individual files 
+// (e.g., user.graphql, product.graphql). 
+// Tools like graphql-tools merge them using mergeTypeDefs().
+export const resolvers = merge ([BookResolver]);
